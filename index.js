db293f7fa2c513edc3966f95374aa20d9a3d1127
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-05-07 14:17:12 +0200

    Added the initial fuel to sub it to get the remaining fuel

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
+let vel = 10000; // velocity (km/h)
 const acc = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
 const d = 0; // distance (km)
@@ -14,13 +14,14 @@ const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const d2 = d + (vel* time /3600) //calcultes new distance
+const rf = fuel - fbr * time //calculates remaining fuel
+const vel2 = calcNewVel(acc, vel, time ) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 function calcNewVel(acc, vel, time) { 
-  return vel + (acc*time)
+  
+  return vel + (acc * time )
 }
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
